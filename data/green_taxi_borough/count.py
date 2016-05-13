eba06ca9d50756e9c104afd6fb9402142a4aa95a
zhangbo0216 <zhangbo0216@hotmail.com>
2016-05-13 01:14:25 -0400

    Add files via upload

---
+++ data/green_taxi_borough/count.py
@@ -0,0 +1,58 @@
+s="part-000"
+Staten_Island=open("Staten_Island","w")
+Queens=open("Queens","w")
+Brooklyn=open("Brooklyn","w")
+Manhattan=open("Manhattan","w")
+Bronx=open("Bronx","w")
+st=[]
+qe=[]
+bk=[]
+mh=[]
+bx=[]
+for i in range(0,71):
+	if (i<=9):
+		i='0'+str(i)
+	else:
+		i=str(i)
+	file = open(s+i)
+	for line in file:	
+		m = line.strip().split(",")
+		if (m[0]=='Staten_Island'):
+			st.append(m[1])
+		elif (m[0]=='Queens'):
+			qe.append(m[1])
+		elif (m[0]=='Brooklyn'):
+			bk.append(m[1])
+		elif (m[0]=='Manhattan'):
+			mh.append(m[1])
+		elif (m[0]=='Bronx'):
+			bx.append(m[1])
+	file.close()
+
+st.sort()
+qe.sort()
+bk.sort()
+mh.sort()
+bx.sort()
+
+for i in st:
+	Staten_Island.write(i)
+	Staten_Island.write('\n')
+for i in qe:
+	Queens.write(i)
+	Queens.write('\n')
+for i in bk:
+	Brooklyn.write(i)
+	Brooklyn.write('\n')
+for i in mh:
+	Manhattan.write(i)
+	Manhattan.write('\n')
+for i in bx:
+	Bronx.write(i)
+	Bronx.write('\n')
+	
+Staten_Island.close()
+Queens.close()
+Brooklyn.close()
+Manhattan.close()
+Bronx.close()
